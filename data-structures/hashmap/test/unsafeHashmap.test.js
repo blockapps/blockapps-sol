17864ee8de1529a2c69385581aeba34cd7a144c8
Samrit Sangal <samrit@samrit.io>
2018-06-01 17:38:54 -0400

    Add hashmap and related testing

---
+++ data-structures/hashmap/test/unsafeHashmap.test.js
@@ -0,0 +1,78 @@
+require('co-mocha');
+const ba = require('blockapps-rest');
+const rest = ba.rest;
+const common = ba.common;
+const config = common.config;
+const util = common.util;
+const assert = common.assert;
+
+const unsafeHashmapJs = require('../unsafeHashmap');
+
+const adminName = util.uid('Admin');
+const adminPassword = '1234';
+
+describe('UnsafeHashmap', function() {
+  this.timeout(config.timeout);
+
+  let admin;
+
+  before(function*() {
+    admin = yield rest.createUser(adminName, adminPassword);
+  });
+
+  it('put', function*() {
+    const hashmap = yield unsafeHashmapJs.uploadContract(admin);
+    const iuid = util.iuid();
+    const args = factory.createEntity(iuid);
+    yield hashmap.put(args);
+    const state = yield hashmap.getState();
+    assert.equal(state.values.length, 2, 'length 2');
+    assert.equal(parseInt(state.values[1]), parseInt(args.value), 'value');
+  });
+
+  it('get', function*() {
+    const hashmap = yield unsafeHashmapJs.uploadContract(admin);
+    const iuid = util.iuid();
+    const args = factory.createEntity(iuid);
+    yield hashmap.put(args);
+    const value = yield hashmap.get({key: args.key});
+    assert.equal(parseInt(value), parseInt(args.value), 'value');
+    const notFound = yield hashmap.get({key: '666'});
+    assert.equal(parseInt(notFound), 0, 'not found');
+  });
+
+  it('contains', function*() {
+    const hashmap = yield unsafeHashmapJs.uploadContract(admin);
+    const iuid = util.iuid();
+    const args = factory.createEntity(iuid);
+    yield hashmap.put(args);
+    const result = yield hashmap.contains({key: args.key});
+    assert.equal(result, true, 'contains: true');
+    const notFound = yield hashmap.contains({key: '666'});
+    assert.equal(notFound, false, 'contains: false');
+  });
+
+  it('size', function*() {
+    const hashmap = yield unsafeHashmapJs.uploadContract(admin);
+    const iuid = util.iuid();
+    const args = factory.createEntity(iuid);
+    yield hashmap.put(args);
+    const size1 = yield hashmap.size();
+    assert.equal(size1, 1, 'size: 1');
+    args.key += 'x';
+    yield hashmap.put(args);
+    const size2 = yield hashmap.size();
+    assert.equal(size2, 2, 'size: 2');
+  });
+});
+
+const factory = {
+  createEntity: factory_createEntity,
+};
+function factory_createEntity(iuid) {
+  const args = {
+    key: 'Key_' + iuid,
+    value: iuid,
+  };
+  return args;
+}
