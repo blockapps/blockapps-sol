f2dc28c5aea6f1f3b5956455842bbec78f3fc058
abhijit sinha <abhijit.sinha@imaginea.com>
2018-07-25 17:50:17 +0530

    Add velidation engine and utility methods

---
+++ validation/contracts/ValidationEngine.sol
@@ -0,0 +1,45 @@
+import "./ValidationRuleInterface.sol";
+import "./ValidationStatus.sol";
+
+contract ValidationEngine is ValidationStatus {
+
+  mapping (bytes32 => bytes32[]) ruleNames;
+  mapping (bytes32 => mapping(bytes32 => mapping(bool => ValidationRuleInterface))) public ruleSets;
+
+  function addRule(bytes32 profileName, bytes32 ruleName, ValidationRuleInterface ruleContractAddress) public {
+    ruleSets[profileName][ruleName][true] = ruleContractAddress;
+
+    bytes32[] names = ruleNames[profileName];
+    names.push(ruleName);
+    ruleNames[profileName] = names;
+  }
+
+  function deactivateRule(bytes32 profileName, bytes32 ruleName) public {
+    ruleSets[profileName][ruleName][false] = ruleSets[profileName][ruleName][true];
+    ruleSets[profileName][ruleName][true]  = ValidationRuleInterface(0);
+  }
+
+  function activateRule(bytes32 profileName, bytes32 ruleName) public {
+    ruleSets[profileName][ruleName][true]  = ruleSets[profileName][ruleName][false];
+    ruleSets[profileName][ruleName][false] = ValidationRuleInterface(0);
+  }
+
+  function validate(address contractAddress, bytes32 profileName) public returns (bool, uint, bytes32) {
+    bytes32[] names = ruleNames[profileName];
+    if(names.length == 0) {
+      return (false, ValidationStatus.PROFILE_INVALID, "Profile Not Found");
+    }
+    for(uint i = 0; i < names.length; i++) {
+      address temp = ruleSets[profileName][names[i]][true];
+      if (temp == 0x0) {
+        continue;
+      }
+      ValidationRuleInterface rule = ValidationRuleInterface(temp);
+      var (isValid, status, message) = rule.apply(contractAddress);
+      if (!isValid) {
+        return (false, status, message);
+      }
+    }
+    return (true, ValidationStatus.VALIDATION_PASSED, "Validations Passed");
+  }
+}
