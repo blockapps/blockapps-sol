bbf325b14700cf736de61f0cc61d2049d8a07e4d
abhijit sinha <abhijit.sinha@imaginea.com>
2018-08-16 21:07:18 +0530

    Update bytes32ToString() to use a more efficient algorithm

--- util/contracts/Util.sol
+++ util/contracts/Util.sol
@@ -11,12 +11,12 @@ contract Util {
   function bytes32ToString(bytes32 x) constant returns (string) {
       bytes memory bytesString = new bytes(32);
       uint charCount = 0;
-      for (uint j = 0; j < 32; j++) {
-          byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-          if (char != 0) {
-              bytesString[charCount] = char;
-              charCount++;
-          }
+      for (charCount = 0; charCount < 32; charCount++) {
+        byte char = byte((uint(x) >> (32 - charCount - 1) * 8) & 0xFF);
+        if (char == 0) {
+          break;
+        }
+        bytesString[charCount] = char;
       }
       bytes memory bytesStringTrimmed = new bytes(charCount);
       for (j = 0; j < charCount; j++) {
